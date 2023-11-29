cf664145f43fc6ac6ba23c11f1488aaa98a49b77
eeneedles <132238481+eeneedles@users.noreply.github.com>
2023-11-28 21:06:17 -0500

    updated Flask code on app.py and code for climate_starter

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,5 @@
 # Import the dependencies.
-
+from flask import Flask, jsonify
 
 
 #################################################
@@ -21,10 +21,33 @@
 #################################################
 # Flask Setup
 #################################################
-
+app=Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    return "Flask is not so bad"
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    #code goes here
+    my_list = [5, 75, 389]
+    return jsonify(my_list)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    #code goes here
+    return jsonify "stations"
+
+@app.route()
+def tobs():
+    #code goes here
+    return jsonify "tobs"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
