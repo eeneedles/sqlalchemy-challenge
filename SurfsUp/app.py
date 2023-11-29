771c6062e1adb6448795c56b278dadee12963af2
eeneedles <132238481+eeneedles@users.noreply.github.com>
2023-11-29 01:53:24 -0500

    updated app.py with code for database set-up, Flask set-up, and Flask routes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -5,18 +5,18 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -30,7 +30,13 @@ app = Flask(__name__)
 #################################################
 @app.route("/")
 def home():
-    return "list all available routes"
+    """List all available api routes."""
+    return(
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+    )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
